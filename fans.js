e0bc96794c8fbd8d084ad3fa1b0a6ddaeb035bee
ZheleznykSerhii <83211761+ZheleznykSerhii@users.noreply.github.com>
2021-10-20 17:17:33 +0300

    fixed inner

--- fans.js
+++ fans.js
@@ -4,16 +4,20 @@ function addPost() {
   let textMessage = document.getElementById('fan-message').value
   let currentDate = new Date()
 
-  document.querySelector('.lastHardcode').insertAdjacentHTML(
-    'afterend',
-    '<div class="text-content text-content-fan "><div>' +
-      textMessage +
-      '</div> <p class="paragraph"></p> <div class="fotter-message flex"><div class="date">' +
-      currentDate.toLocaleString([], {
-        dateStyle: 'short',
-        timeStyle: 'short',
-      }) +
-      '</div><div class="username">Guest</div></div></div>'
-  )
+  if (textMessage.length < 2) {
+    alert('Введіть більше одного символу')
+  } else {
+    document.querySelector('.lastHardcode').insertAdjacentHTML(
+      'beforeend',
+      '<div class="text-content text-content-fan "><div>' +
+        textMessage +
+        '</div> <p class="paragraph"></p> <div class="fotter-message flex"><div class="date">' +
+        currentDate.toLocaleString([], {
+          dateStyle: 'short',
+          timeStyle: 'short',
+        }) +
+        '</div><div class="username">Guest</div></div></div>'
+    )
+  }
   document.getElementById('fan-message').value = null
 }
