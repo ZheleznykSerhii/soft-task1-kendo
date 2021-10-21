26ac76efe4c727dd880abdd7a07294cc92b7bf1c
ZheleznykSerhii <83211761+ZheleznykSerhii@users.noreply.github.com>
2021-10-21 20:01:15 +0300

    fixed bug with insertAdjacentHTML

--- fans.js
+++ fans.js
@@ -2,16 +2,24 @@ document.querySelector('button').onclick = addPost
 
 function addPost() {
   let textMessage = document.getElementById('fan-message').value
+
   let currentDate = new Date()
 
-  if (textMessage.length < 2) {
-    alert('Введіть більше одного символу')
+  if (textMessage.length < 1 || textMessage.trim() == '') {
+    alert('Введіть мінімум один символ')
+  } else if (
+    (textMessage.indexOf('>') > -1 && textMessage.indexOf('<') > -1) ||
+    textMessage.indexOf('<') === 0
+  ) {
+    alert(
+      'Будь ласка, не використовуйте теги у коментарях. Я з часом перепишу через map список повідомлень і тоді можна буде використовувати <>'
+    )
   } else {
     document.querySelector('.lastHardcode').insertAdjacentHTML(
       'beforeend',
-      '<div class="text-content text-content-fan "><div>' +
+      '<div class="text-content text-content-fan "><div></div> <p class="paragraph">' +
         textMessage +
-        '</div> <p class="paragraph"></p> <div class="fotter-message flex"><div class="date">' +
+        '</p> <div class="fotter-message flex"><div class="date">' +
         currentDate.toLocaleString([], {
           dateStyle: 'short',
           timeStyle: 'short',
