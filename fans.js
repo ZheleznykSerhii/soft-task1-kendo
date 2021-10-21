66bb28c3260dd60ec4aa3bc2ac62178b9d268f82
ZheleznykSerhii <83211761+ZheleznykSerhii@users.noreply.github.com>
2021-10-21 21:08:20 +0300

    try to fix

--- fans.js
+++ fans.js
@@ -3,14 +3,13 @@ document.querySelector('button').onclick = addPost
 function addPost() {
   let textMessage = document.getElementById('fan-message').value
 
+  let currentDate = new Date()
+
   if (textMessage.length < 1 || textMessage.trim() == '') {
     alert('Введіть мінімум один символ')
-  } else if (
-    (textMessage.indexOf('>') > -1 && textMessage.indexOf('<') > -1) ||
-    textMessage.indexOf('<') === 0
-  ) {
+  } else if (textMessage.indexOf('>') > -1 && textMessage.indexOf('<') > -1) {
     alert(
-      'Будь ласка, не використовуйте у одному коментарі обидва символи "<" ">" одночасно, не починайте коментар с символу "<". Я з часом перепишу через map список повідомлень і тоді можна буде використовувати <>'
+      'Будь ласка, не використовуйте теги у коментарях. Я з часом перепишу через map список повідомлень і тоді можна буде використовувати <>'
     )
   } else {
     document.querySelector('.lastHardcode').insertAdjacentHTML(
@@ -18,7 +17,7 @@ function addPost() {
       '<div class="text-content text-content-fan "><div></div> <p class="paragraph">' +
         textMessage +
         '</p> <div class="fotter-message flex"><div class="date">' +
-        new Date().toLocaleString([], {
+        currentDate.toLocaleString([], {
           dateStyle: 'short',
           timeStyle: 'short',
         }) +
