67910eec591dd7bb97af0354f61125404fcab526
ZheleznykSerhii <83211761+ZheleznykSerhii@users.noreply.github.com>
2021-10-21 20:36:41 +0300

    deleted the line after comment

--- fans.js
+++ fans.js
@@ -3,8 +3,6 @@ document.querySelector('button').onclick = addPost
 function addPost() {
   let textMessage = document.getElementById('fan-message').value
 
-  let currentDate = new Date()
-
   if (textMessage.length < 1 || textMessage.trim() == '') {
     alert('Введіть мінімум один символ')
   } else if (
@@ -12,15 +10,15 @@ function addPost() {
     textMessage.indexOf('<') === 0
   ) {
     alert(
-      'Будь ласка, не використовуйте теги у коментарях. Я з часом перепишу через map список повідомлень і тоді можна буде використовувати <>'
+      'Будь ласка, не використовуйте у одному коментарі обидва символи "<" ">" одночасно, не починайте коментар с символу "<". Я з часом перепишу через map список повідомлень і тоді можна буде використовувати <>'
     )
   } else {
-    document.querySelector('.lastHardcode').insertAdjacentHTML(
+    document.querySelector('.afterHardcode').insertAdjacentHTML(
       'beforeend',
       '<div class="text-content text-content-fan "><div></div> <p class="paragraph">' +
         textMessage +
         '</p> <div class="fotter-message flex"><div class="date">' +
-        currentDate.toLocaleString([], {
+        new Date().toLocaleString([], {
           dateStyle: 'short',
           timeStyle: 'short',
         }) +
